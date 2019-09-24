6438be72925186576bf8869bfb92f0ee17d5bb1e
AndreyBodulin <55592933+AndreyBodulin@users.noreply.github.com>
2019-09-24 20:49:02 +0400

    Add files via upload
    Исправил функцию move, сделал столкновение танка с пулей

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once('logic/Logic.php');
 
 $logic = new Logic();
-$logic->move(0, 'left');
+$logic->move(321, 'up');
 print_r($logic->getTanks());
 
 /* 1) игровое поле (поверхности и преграды, разрушаемость)
--- logic/Logic.php
+++ logic/Logic.php
@@ -1,5 +1,6 @@
 <?php
 require_once('types/Tank.php');
+require_once('types/Bullet.php');
 
 class Logic {
     function __construct() {
@@ -12,15 +13,23 @@ class Logic {
         $this->tanks[] = new Tank($data);
         $data = new stdClass();
         $data->id = 321;
-        $data->x = 1;
-        $data->y = 0;
+        $data->x = 2;
+        $data->y = 2;
         $this->tanks[] = new Tank($data);
         // add field
         $this->field = array(array(0, 0, 0),
-                             array(1, 0, 0),
+                             array(0, 0, 0),
                              array(0, 0, 0)
                             );
-    }
+
+		// add bullet
+		$data = new stdClass();
+		$data->id = 111;
+		$data->x = 0;
+		$data->y = 0;
+		$this->bullets = array();
+		$this->bullets[] = new Bullet($data);
+	}
 
     private function findTank($x, $y) {
         for ($i = 0; $i < count($this->tanks); $i++) {
@@ -31,6 +40,14 @@ class Logic {
         return null;
     }
 
+	private function killTank($tank) {
+		for ($i = 0; $i < count($this->tanks); $i++) {
+			if ($tank == $this->tanks[$i]) {
+				array_splice($this->tanks, $i, 1);	
+			}
+		}
+	}
+
     public function getTanks(){
         return $this->tanks;
     }
@@ -39,21 +56,40 @@ class Logic {
         return $this->field;
     }
 
-    public function move($num, $direction) {
-        $tank = $this->tanks[$num];
-        $x = $tank->x;
-        $y = $tank->y;
-        switch ($direction) {
-            case 'left': $x--; break;
-            case 'right': $x++; break;
-            case 'up': $y--; break;
-            case 'down': $y++; break;
-        }
-        if ($this->findTank($x, $y) || $this->field[$y][$x] > 0) {
-            return false;
-        }
-        $tank->x = $x;
-        $tank->y = $y;
-        return true;
-    }
+	public function getBullets() {
+		return $this->bullets;
+	}
+
+    public function move($id, $direction) {
+		if (isset($id)) {
+			
+			for ($i = 0; $i < count($this->tanks); $i++) {
+				if ($this->tanks[$i]->id == $id) {
+					$tank = $this->tanks[$i];
+				}
+			}
+			$x = $tank->x;
+			$y = $tank->y;
+			switch ($direction) {
+				case 'left': $x--; break;
+				case 'right': $x++; break;
+				case 'up': $y--; break;
+				case 'down': $y++; break;
+			}
+			if ($this->findTank($x, $y) || $this->field[$y][$x] > 0) {
+				return false;
+			}
+			for ($i = 0; $i < count($this->bullets); $i++){
+				if ($x == $this->bullets[$i]->x  && $y == $this->bullets[$i]->y) {
+					$this->killTank($tank);
+					return false;
+				}
+			}
+
+			$tank->x = $x;
+			$tank->y = $y;
+			return true;
+		}
+		return false;
+	}
 }
