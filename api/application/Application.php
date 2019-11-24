56f1ab9f81128bb1ee89014a8ea3552bf512697a
AndreyBodulin <55592933+AndreyBodulin@users.noreply.github.com>
2019-11-24 19:06:03 +0400

    Add files via upload
    rnd X Y tanka

---
+++ api/application/Application.php
@@ -0,0 +1,104 @@
+<?php
+require_once('db/DB.php');
+require_once('user/User.php');
+require_once('vmech/VMech.php');
+
+class Application {
+
+    function __construct() {
+        $db = new DB();
+        $this->user = new User($db);
+        $this->vMech = new VMech($db);
+    }
+
+    /********************/
+    /* Про пользователя */
+    /********************/
+    public function login($params) {
+        if ($params['login'] && $params['hash'] && intval($params['rnd'])) {
+            return $this->user->login($params['login'], $params['hash'], intval($params['rnd']));
+        }
+        return false;
+    }
+
+    public function logout($params) {
+        if ($params['token']) {
+            return $this->user->logout($params['token']);
+        }
+        return false;
+    }
+
+    public function registration($params) {
+		if ($params['login'] && $params['hash']) {
+			return $this->user->registration($params['login'], $params['hash']);
+		}
+		return false;
+    }
+
+    /************/
+    /* Про игру */
+    /************/
+
+    public function addTank($params) {
+        if ($params['token'] && 
+            $params['team'] && 
+            $params['hull'] && 
+            $params['gun'] && 
+            $params['shassis'] &&
+            $params['money']
+        ) {
+            $user = $this->user->getUserByToken($params['token']);
+            if ($user) {
+                return $this->vMech->addTank(
+                    $user->id, 
+                    $params['team'], 
+                    $params['hull'],
+                    $params['gun'],
+                    $params['shassis'],
+                    $user->money);
+            }
+        }
+        return false;
+    }
+
+    public function move($params) {
+        if ($params['token'] && $params['direction']) {
+            $user = $this->user->getUserByToken($params['token']);
+            if ($user) {
+                return $this->vMech->move($user->id, $params['direction']);
+            }
+        }
+        return false;
+    }
+
+    public function shoot($params) {
+        if ($params['token']) {
+            $user = $this->user->getUserByToken($params['token']);
+            if ($user) {
+                return $this->vMech->shoot($user->id);
+            }
+        }
+        return false;
+    }
+
+
+    public function updateScene($params) {
+        if ($params['token']) { 
+            $user = $this->user->getUserByToken($params['token']);
+            if ($user) {
+                return $this->vMech->updateScene($user->id);
+            }
+        }
+        return false;
+    }
+
+    public function joinGame($params) {
+        if($params['id'] && $params['x'] && $params['y'] && $params['hull'] && $params['gun'] && $params['shasshi']){
+            return $this->vMech->tanks[] = $this->vMech->createTank(intval($params['id']), intval($params['x']), intval($params['y']), $params['hull'], $params['gun'], $params['shasshi']);
+        } return false;
+    }
+
+    public function getConstructor() {
+        return $this->vMech->getConstructor();
+    }
+}
